37b8745876caea13d9a77ae404e75dbeea7dd0e5
nied <sebastian.nied@umweltbundesamt.at>
2026-01-29 20:13:39 +0100

    fixing import issues

---
+++ src/ml_template/core.py
@@ -0,0 +1,8 @@
+import pandas as pd
+
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import StandardScaler
+from sklearn.pipeline import Pipeline
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
+
+import xgboost as xgb
